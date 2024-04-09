5732c3ba6dd14db4b666bc52c825b76a4b10a313
Reynaldi Neo Ramadhani <115524002+reynaldineo@users.noreply.github.com>
2024-04-09 22:53:01 +0700

    feat: add footer (#14)

--- src/app/sandbox/layout/page.tsx
+++ src/app/sandbox/layout/page.tsx
@@ -2,7 +2,7 @@ import MainLayout from '@/components/layout/MainLayout';
 
 export default function SanboxLayout() {
   return (
-    <MainLayout withNavbar>
+    <MainLayout withNavbar withFooter>
       <section className='min-h-screen bg-base-500'></section>
       <section className='min-h-screen bg-secondary-500'></section>
     </MainLayout>
