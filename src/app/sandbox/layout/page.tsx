f8dcdf82d76fd479c9fe2a6f39921ab6ea4fe67f
Reynaldi Neo Ramadhani <115524002+reynaldineo@users.noreply.github.com>
2024-04-09 17:05:58 +0700

    feat: add navbar (#12)
    * feat: add navbar
    
    * style: update button variant

---
+++ src/app/sandbox/layout/page.tsx
@@ -0,0 +1,10 @@
+import MainLayout from '@/components/layout/MainLayout';
+
+export default function SanboxLayout() {
+  return (
+    <MainLayout withNavbar>
+      <section className='min-h-screen bg-base-500'></section>
+      <section className='min-h-screen bg-secondary-500'></section>
+    </MainLayout>
+  );
+}
---
+++ src/contents/navbar.ts
@@ -0,0 +1,15 @@
+type NavbarLinks = {
+  name: string;
+  href: string;
+}[];
+
+export const navbarLinks: NavbarLinks = [
+  {
+    name: 'Shop',
+    href: '/',
+  },
+  {
+    name: 'Course',
+    href: '/',
+  },
+];
