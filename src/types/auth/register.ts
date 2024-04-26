5a8676208aa793a885ead2efe78572212bbea6c8
Reynaldi Neo Ramadhani <115524002+reynaldineo@users.noreply.github.com>
2024-04-27 01:12:50 +0700

    feat: integrate register (#26)

---
+++ src/types/auth/register.ts
@@ -0,0 +1,11 @@
+export type RegisterForm = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export type RegisterResponse = {
+  id: string;
+  email: string;
+  username: string;
+};
